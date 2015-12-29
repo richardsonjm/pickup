f9ddbfb0989f08cfc9ea06af0e6167937f26bdc9
John Richardson <richardsonjm@gmail.com>
2015-12-28 19:18:14 -0500

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  normalize_attributes :email, :firstname, :lastname
+  validates_presence_of :email, :auth_token
+
+  after_initialize :set_auth_token
+
+  validates :email, presence: true, uniqueness: {case_sensitive: false},
+    format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates_uniqueness_of :auth_token
+
+  TOKEN_LENGTH = 16
+
+  private
+
+  def set_auth_token
+    self.auth_token ||= generate_auth_token
+  end
+
+  def generate_auth_token
+    SecureRandom.hex(TOKEN_LENGTH)
+  end
+end
